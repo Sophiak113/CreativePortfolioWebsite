e2154cb93ed4a7fdb5f0e8e28199ce9a5a33f6a1
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-08-24 14:25:12 +0100

    Put intropg2 items into correct layout

--- my-space-portfolio/src/App.jsx
+++ my-space-portfolio/src/App.jsx
@@ -7,9 +7,6 @@ import TitleBar from "./components/TitleBar";
 import IntroPg2 from "./components/IntroPg2";
 
 function App() {
-  const handleSelectedIcon = (currentHeading) =>{
-    console.log(currentHeading)
-  }
   return (
     <div className="main-container">
     
@@ -20,7 +17,7 @@ function App() {
           <div className="planet-wrapper">
             {/* Could I create icons as a list as they repeat? */}
               <Icon src = "/PinkSolarSystem.png" alt="Pink solar system"
-              position={{}} heading = "Intro" onSelectIcon={handleSelectedIcon()}/>
+              position={{}} heading = "Intro"/>
               {/* Left position tag empty, just adding images before deciding where they'll go */}
               <Icon src = "/PurpleSolarSystem.png" alt="Purple solar system"
               position={{}} heading = "Projects"/>
--- my-space-portfolio/src/components/IntroPg2.jsx
+++ my-space-portfolio/src/components/IntroPg2.jsx
@@ -5,24 +5,28 @@ import Section from './Section'
 const IntroPg2 = () => {
   return (
     <div className='intro-container2'>
-      <TitleBar id= 'intro2-1' page='Hobbies'></TitleBar>
-      <div className="hobbieName">
-        <Section content='hobbie Name'></Section>
-      </div>
-      <div className="hobbieRating">
-        <img src="/PinkSolarSystem.png" alt="1" />
-        <img src="/PinkSolarSystem.png" alt="2" />
-        <img src="/PinkSolarSystem.png" alt="3" />
 
-      </div>
-      <div className="hobbieIcon">
-        <img src="/GreenSolarSystem.png" alt="green" />
-      </div>
-      <div className="rightArrow"></div>
-      <div className="leftArrow"></div>
-      <div className='introPlanet'>
-        <img src="/PurpleSolarSystem.png" alt="anchor planet" />
-      </div>
+          <div>
+              <TitleBar id= 'intro2-1' page='Hobbies'></TitleBar>
+          </div>
+          <div className="hobbieName">
+            <Section content='hobbie Name'></Section>
+          </div>
+          <div className='intro2-carousel'>
+              <div className="leftArrow"></div>
+              <div className="hobbieRating">
+                <img src="/PinkSolarSystem.png" alt="1" />
+                <img src="/PinkSolarSystem.png" alt="2" />
+                <img src="/PinkSolarSystem.png" alt="3" />
+              </div>
+              <div className="rightArrow"></div>
+          </div>
+
+          <div className='introPlanet'>
+            <img src="/PurpleSolarSystem.png" alt="anchor planet" />
+          </div>
+
+
     </div>
   )
 }
